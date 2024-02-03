54b8d12681b4d10e92a1e44ad685b3174ec270c5
sonangrai <shonahangrae@gmail.com>
2024-02-03 15:51:02 +0545

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 function page() {
-  return <div>Welcome to My DSA solutions</div>;
+  return (
+    <div className="flex flex-col justify-center gap-2 min-h-[600px]">
+      <p className="text-[4em]">
+        Welcome to My <br />
+        <span className="text-blue-500 font-bold">DSA Solutions</span>{" "}
+        <sup>JS</sup>
+      </p>
+    </div>
+  );
 }
 
 export default page;
