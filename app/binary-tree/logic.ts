9a4c5dc63eaf4337ddfb7e505f6d84187e7b231f
sonangrai <shonahangrae@gmail.com>
2024-08-17 09:27:01 +0545

    feat: delete node bst function

--- app/binary-tree/logic.ts
+++ app/binary-tree/logic.ts
@@ -104,4 +104,45 @@ export class BinaryTree {
     }
     return current;
   }
+
+  remove(data: number) {
+    function removeNode(node, data) {
+      if (node == null) return null; // BST is empty
+
+      if (node.data == data) {
+        if (node.left == null && node.right == null) {
+          return null; // Only one level
+        }
+
+        if (node.left == null) {
+          return node.right;
+        }
+
+        if (node.right == null) {
+          return node.left;
+        }
+
+        // Both side consist of node
+        let tempNode;
+        // Finding smallest node in right side
+        let min = node.right;
+        while (min?.left != null) {
+          min = min.left;
+        }
+        tempNode = min;
+
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (node.data > data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    }
+
+    return removeNode(this.root, data);
+  }
 }
