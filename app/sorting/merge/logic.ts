a60427516f7023929700dcb31ff50cb5516e008a
sonangrai <shonahangrae@gmail.com>
2025-01-01 19:44:29 +0545

    feat: merge sort logic

--- app/sorting/merge/logic.ts
+++ app/sorting/merge/logic.ts
@@ -1,7 +1,43 @@
-const mergeSortFn = (input: number[]) => {
-  let inp = [38, 27, 43, 10];
+const merge = (left: number[], right: number[]) => {
+  const result: number[] = [];
 
-  return inp.join(",");
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  while (i < left.length) {
+    result.push(left[i]);
+    i++;
+  }
+
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
+};
+
+const mergeSortFn = (input: number[]) => {
+  if (input.length <= 1) return input;
+
+  const middle = Math.floor(input.length / 2);
+  const left = input.slice(0, middle);
+  const right = input.slice(middle);
+
+  const sortedLeft = mergeSortFn(left);
+  const sortedRight = mergeSortFn(right);
+
+  return merge(sortedLeft, sortedRight);
 };
 
 export default mergeSortFn;
